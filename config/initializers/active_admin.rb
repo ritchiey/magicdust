04b2dc121e40abeea17c61e9d4b08617565644eb
Ritchie Young <ritchiey@gmail.com>
2013-01-04 13:09:23 +0800

    Disable authentication for now

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -10,7 +10,7 @@ ActiveAdmin.setup do |config|
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
-  # config.site_title_link = "/"
+   config.site_title_link = "/"
 
   # Set an optional image to be displayed for the header
   # instead of a string (overrides :site_title)
@@ -55,7 +55,8 @@ ActiveAdmin.setup do |config|
   #
   # This setting changes the method which Active Admin calls
   # within the controller.
-  config.authentication_method = :authenticate_admin_user!
+  #config.authentication_method = :authenticate_admin_user!
+  config.authentication_method = false
 
 
   # == Current User
@@ -65,7 +66,8 @@ ActiveAdmin.setup do |config|
   #
   # This setting changes the method which Active Admin calls
   # to return the currently logged in user.
-  config.current_user_method = :current_admin_user
+  #config.current_user_method = :current_admin_user
+  config.current_user_method = false
 
 
   # == Logging Out
@@ -78,7 +80,8 @@ ActiveAdmin.setup do |config|
   # will call the method to return the path.
   #
   # Default:
-  config.logout_link_path = :destroy_admin_user_session_path
+  #config.logout_link_path = :destroy_admin_user_session_path
+  config.logout_link_path = false
 
   # This setting changes the http method used when rendering the
   # link. For example :get, :delete, :put, etc..
